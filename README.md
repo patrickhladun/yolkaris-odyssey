83c4bc274354c855c1bbca928a9ca76b2683055e
Patrick Hladun <patrickhladun@gmail.com>
2023-12-30 19:07:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python Adventure Game - CI Project 3
---
+++ run.py
@@ -0,0 +1,14 @@
+# Terminal size is 80 characters wide and 24 rows high
+
+def main_story():
+    print("This is the main story")
+
+def side_story():
+    print("This is the side story")
+    
+def main():
+    print("Hello Player!")
+    main_story()
+
+if __name__ == "__main__":
+    main()
