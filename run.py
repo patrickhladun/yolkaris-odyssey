b6fc1f305d470c0509fbf60d58505da313fa072f
Patrick Hladun <patrickhladun@gmail.com>
2024-01-01 17:20:04 +0000

    refactor: move static modules outside of the Game class

--- run.py
+++ run.py
@@ -2,16 +2,19 @@ from game.config import Config
 from art import *
 from utils import text, space, clear_terminal
 
+
 class Character:
     def __init__(self, name) -> None:
         self.name = name
+
 
 class Player(Character):
     def __init__(self, name) -> None:
         super().__init__(name)
 
+
 class Location:
-    def __init__(self, name, description, size) -> None:
+    def __init__(self, name: str, description: str, size: tuple) -> None:
         self.name = name
         self.description = description
         self.size = size
@@ -29,27 +32,95 @@ class Location:
                 print(char, end="")
             print()  # New line after each row
         print()  # Extra line for spacing
-        
+
     def is_valid_position(self, position) -> bool:
         x, y = position
         return 0 <= x < self.size[0] and 0 <= y < self.size[1]
+
 
 class Yolkaris(Location):
     def __init__(self) -> None:
         super().__init__("Yolkaris", "A vibrant planet with diverse ecosystems.", (18, 7))
 
+
 class Mystara(Location):
     def __init__(self) -> None:
         super().__init__("Mystara", "A mysterious planet covered in thick jungles.", (2, 2))
+
 
 class Luminara(Location):
     def __init__(self) -> None:
         super().__init__("Luminara", "A radiant planet with a luminous landscape.", (2, 2))
 
-class Game():
+
+def game_title() -> None:
+    """
+    This is the main menu.
+    """
+    yolkaris = text2art("Yolkaris", font="dos_rebel", chr_ignore=True)
+    odyssey = text2art("Odyssey", font="dos_rebel", chr_ignore=True)
+    print(yolkaris)
+    print(odyssey)
+
+
+def show_help() -> None:
+    clear_terminal()
+    text("Available Commands:", space=1)
+    text("  north      - Move North (up)", delay=0.1)
+    text("  south      - Move South (down)", delay=0.1)
+    text("  east       - Move East (up)", delay=0.1)
+    text("  west       - Move West (up)", delay=0.1, space=1)
+    text("  help       - Show this help message", delay=0.1)
+    text("  quit       - Quit the game", delay=0.1)
+
+
+def intro() -> None:
+    """
+    This is the introduction to the game.
+    """
+    clear_terminal()
+
+    text("In the boundless expanse of the cosmos, among a sea of twinkling stars,")
+    text("lies Yolkaris - a vibrant and lively planet home to an extraordinary species")
+    text("of spacefaring chickens. But now, Yolkaris faces an unprecedented crisis.")
+    text("A mysterious and malevolent cosmic dust, known as The Dark Dust, has")
+    text("enshrouded the planet in shadow, blocking the essential sunlight and")
+    text("disrupting the delicate balance of its ecosystem. The once bright and")
+    text("bustling world, a haven of clucking harmony, now teeters on the brink")
+    text("of ecological collapse.", delay=0.6, space=1)
+
+    input("Press ENTER to continue ... ")
+    clear_terminal()
+
+    text("In this hour of desperation, hope rests on the wings of one brave hero - Clucky.")
+    text("Renowned for courage and cleverness, Clucky's destiny is to embark on a quest")
+    text("beyond the stars. The mission is dangerous, the stakes are high, and the journey")
+    text("will take Clucky to uncharted corners of the galaxy.", delay=0.6, space=1)
+
+    text("As Clucky, you will traverse through cosmic wonders and confront unknown dangers.")
+    text("Your quest will lead you to ancient relics and forgotten worlds, where secrets")
+    text("of The Dark Dust await to be uncovered. The journey promises challenges, trials,")
+    text("and the chance to become the saviour that Yolkaris desperately needs.", delay=0.6, space=1)
+
+    input("Press ENTER to continue ... ")
+    clear_terminal()
+
+    text("Do you have the courage to step into Clucky's shoes? Are you ready to soar beyond the stars,")
+    text("unravel the mystery of The Dark Dust, and find the key to save your beloved planet?")
+    text("Your decisions, bravery, and wits will shape the fate of Yolkaris.", delay=0.6, space=1)
+
+    text("Prepare for an odyssey that spans the cosmos - an adventure where your actions will determine")
+    text("the survival of an entire world. The journey of Yolkaris Odyssey begins now, and the destiny")
+    text("of a planet rests in your wings.", delay=0.6, space=1)
+
+    input("Press ENTER to embark on your journey and become the hero Yolkaris needs... ")
+
+
+class Game:
     """
     This is the main class for the game.
     """
+
     def __init__(self) -> None:
         self.location_objects = {
             "Yolkaris": Yolkaris(),
@@ -79,74 +150,14 @@ class Game():
         clear_terminal()
         text("Player Stats:")
         text(f"Name: {player.name}")
-    
+
         text(f"\nCurrent Location: {current_location.name}")
         text(f"{current_location.description}\n")
-
-    def game_title(self) -> None:
-        """
-        This is the main menu.
-        """
-        yolkaris=text2art("Yolkaris", font="dos_rebel", chr_ignore=True)
-        odyssey=text2art("Odyssey", font="dos_rebel", chr_ignore=True)
-        print(yolkaris)
-        print(odyssey)
-
-    def intro(self) -> None:
-        """
-        This is the introduction to the game.
-        """
-        clear_terminal()
-        
-        text("In the boundless expanse of the cosmos, among a sea of twinkling stars,")
-        text("lies Yolkaris - a vibrant and lively planet home to an extraordinary species")
-        text("of spacefaring chickens. But now, Yolkaris faces an unprecedented crisis.")
-        text("A mysterious and malevolent cosmic dust, known as The Dark Dust, has")
-        text("enshrouded the planet in shadow, blocking the essential sunlight and")
-        text("disrupting the delicate balance of its ecosystem. The once bright and")
-        text("bustling world, a haven of clucking harmony, now teeters on the brink")
-        text("of ecological collapse.", delay=0.6, space=1)
-
-        input("Press ENTER to continue ... ")
-        clear_terminal()
-
-        text("In this hour of desperation, hope rests on the wings of one brave hero - Clucky.")
-        text("Renowned for courage and cleverness, Clucky's destiny is to embark on a quest")
-        text("beyond the stars. The mission is dangerous, the stakes are high, and the journey")
-        text("will take Clucky to uncharted corners of the galaxy.", delay=0.6, space=1)
-
-        text("As Clucky, you will traverse through cosmic wonders and confront unknown dangers.")
-        text("Your quest will lead you to ancient relics and forgotten worlds, where secrets")
-        text("of The Dark Dust await to be uncovered. The journey promises challenges, trials,")
-        text("and the chance to become the saviour that Yolkaris desperately needs.", delay=0.6, space=1)
-        
-        input("Press ENTER to continue ... ")
-        clear_terminal()
-
-        text("Do you have the courage to step into Clucky's shoes? Are you ready to soar beyond the stars,")
-        text("unravel the mystery of The Dark Dust, and find the key to save your beloved planet?")
-        text("Your decisions, bravery, and wits will shape the fate of Yolkaris.", delay=0.6, space=1)
-
-        text("Prepare for an odyssey that spans the cosmos - an adventure where your actions will determine")
-        text("the survival of an entire world. The journey of Yolkaris Odyssey begins now, and the destiny")
-        text("of a planet rests in your wings.", delay=0.6, space=1)
-
-        input("Press ENTER to embark on your journey and become the hero Yolkaris needs... ")
-
-    def show_help(self) -> None:
-        clear_terminal()
-        text("Available Commands:", space=1)
-        text("  north      - Move North (up)", delay=0.1)
-        text("  south      - Move South (down)", delay=0.1)
-        text("  east       - Move East (up)", delay=0.1)
-        text("  west       - Move West (up)", delay=0.1, space=1)
-        text("  help       - Show this help message", delay=0.1)
-        text("  quit       - Quit the game", delay=0.1)
 
     def choose_action(self) -> None:
         action = input(": ")
         if action == "help":
-            self.show_help()
+            show_help()
         elif action == "map":
             self.display_map()
         elif action == "north":
@@ -154,9 +165,9 @@ class Game():
         elif action == "south":
             self.move_south()
         elif action == "east":
-            self.move_east() 
+            self.move_east()
         elif action == "west":
-            self.move_west()               
+            self.move_west()
         elif action == "stats":
             self.show_player_stats()
         elif action == "quit":
@@ -167,7 +178,7 @@ class Game():
         This is the main game loop.
         """
         clear_terminal()
-        self.game_title()
+        game_title()
         text("Welcome to Yolkaris Odyssey! v0.1")
         text("This is a text-based adventure game.")
         input("Press ENTER to start the game ...")
@@ -178,7 +189,7 @@ class Game():
         self.assign_player_to_location()
         input("Press ENTER to continue ... ")
         # Start game intro
-        self.intro()
+        intro()
         while not self.game_over:
             self.choose_action()
 
@@ -221,7 +232,7 @@ class Game():
             self.get_current_location()
             self.choose_action()
 
-    def update_player_position(self, dx, dy) -> None:
+    def update_player_position(self, dx: int, dy: int) -> None:
         current_location = self.get_current_location()
         x, y = current_location.player_position
         new_position = (x + dx, y + dy)
@@ -229,7 +240,7 @@ class Game():
         if current_location.is_valid_position(new_position):
             current_location.player_position = new_position
         else:
-            print("You can't move in that direction.")       
+            print("You can't move in that direction.")
 
     def move_north(self) -> None:
         self.update_player_position(0, -1)
@@ -239,9 +250,10 @@ class Game():
 
     def move_east(self) -> None:
         self.update_player_position(1, 0)
-    
+
     def move_west(self) -> None:
         self.update_player_position(-1, 0)
+
 
 if __name__ == "__main__":
     game = Game()
