9a43e673a6c4dbe7643e10c04609146703d94d20
Patrick Hladun <patrickhladun@gmail.com>
2024-01-01 11:52:30 +0000

    refactor: move functions to utils folder

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import os
-import time
-
 from game.config import Config
-
-# Terminal size is 80 characters wide and 24 rows high
+from utils.text_utils import text, space, clear_terminal
 
 class Character:
     def __init__(self, name, description):
@@ -78,26 +74,6 @@ class Game():
             self.intro()
             input("Press enter to exit ...")
             self.game_over = True
-
-def text(text, delay=0.2, space=0):
-    """
-    This prints text to the terminal.
-    - text: the text to print
-    - delay: the delay between each character
-    - space: the number of new lines to print after the text
-    """
-    line_space = '\n' * space
-    print(text + line_space)
-    time.sleep(delay)
-
-def space(space=1,delay=0.2):
-    """
-    This prints a new line to the terminal.
-    - space: the number of new lines to print
-    - delay: the delay between each new line
-    """
-    print('\n')
-    time.sleep(delay)
 
 if __name__ == "__main__":
     game = Game()
