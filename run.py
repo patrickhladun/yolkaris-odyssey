088e2132689169bddc0515a62f273d1141204671
Patrick Hladun <patrickhladun@gmail.com>
2024-01-01 11:42:04 +0000

    feat: add custom text and space functions to format text

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import os
 import time
+
+from game.config import Config
+
 # Terminal size is 80 characters wide and 24 rows high
 
 class Character:
@@ -18,18 +21,51 @@ class Game():
     def __init__(self):
         self.game_over = False
         self.player = None
-        self.text_speed = 0.4
+        self.config = Config()
 
     def create_player(self):
         """
         This creates the player.
         """
-        print_text("Welcome to the game!", self.text_speed)
+        text("Welcome to the game!", self.config.text_delay)
         name = input("What is your name? ")
         description = input("Write about yourself: ")
         self.player = Player(name, description)
-        print_text(f"Welcome {self.player.name}!", self.text_speed)
-        print_text("Good luck!", self.text_speed)
+        text(f"Welcome {self.player.name}!", self.config.text_delay)
+
+    def intro(self):
+        """
+        This is the introduction to the game.
+        """
+        
+        text("In the far reaches of the galaxy, nestled among the stars, lies a world")
+        text("unlike any other - the planet Yolkaris. A world brimming with mystery and")
+        text("adventure, where legends speak of an ancient power hidden deep within its")
+        text("vibrant landscapes.", delay=0.6, space=1)
+        
+        text("You are Clucky, an intrepid space chicken from the distant planet of Aviara.")
+        text("Your journey has brought you to Yolkaris on a quest of great importance. The")
+        text("fate of your home planet hangs in the balance, threatened by a mysterious")
+        text("force known only as The Dark Dust.", delay=0.6, space=1)
+
+        text("The Dark Dust is a cosmic entity that has plagued the galaxy for millennia,")
+        text("devouring entire worlds and leaving nothing but darkness in its wake. It is")
+        text("said that the only power capable of stopping The Dark Dust is the legendary")
+        text("Aurora Orb, a relic of immense power hidden somewhere on Yolkaris.", delay=0.6, space=1)
+
+        text("You must find the Aurora Orb and use its power to save Aviara from certain")
+        text("destruction. But you are not alone in your quest. You will be joined by")
+        text("your trusty sidekick, Eggy, a small robot with a big heart.", delay=0.6, space=1)
+
+        text("Together, you will explore the vast landscapes of Yolkaris, uncovering its")
+        text("secrets and discovering the truth behind the legend of the Aurora Orb.", delay=0.6, space=1)
+
+        text("will you rise to the challenge and become the hero that Aviara needs? Or will")
+        text("the secrets of Yolkaris remain locked away forever, shrouded in the shadows")
+        text("of the cosmos?", delay=0.6, space=1)
+        
+        text("The adventure begins now, and the destiny of a world rests in your wings.",space=0)
+        text("Good luck!")
 
     def start_game(self):
         """
@@ -37,12 +73,19 @@ class Game():
         """
         self.create_player()
         while not self.game_over:
-            print_text("Welcome to the game!", self.text_speed)
+            text("Welcome to the game!")
+            input("Press enter to exit ...")
+            self.intro()
             input("Press enter to exit ...")
             self.game_over = True
 
-def print_text(text, delay=0.2):
-    print(text)
+def text(text, delay=0.2, space=0):
+    line_space = '\n' * space
+    print(text + line_space)
+    time.sleep(delay)
+
+def space(space=1,delay=0.2):
+    print('\n')
     time.sleep(delay)
 
 if __name__ == "__main__":
